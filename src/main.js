4ae391a783a13417d26e8c1d326c449f0b9fdd56
maysasfour <mays.suhail@gmail.com>
2023-09-26 18:02:08 +0300

    update project

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import data from './data.json';
+import CardComp from './card';
+import './main.css';
+
+function Main(){
+    return (
+        <>
+        <div id="container">
+      
+       {data.map(function(item)       {
+        return(
+
+        <CardComp image={item.image_url} title={item.title} description={item.description}/>
+       )
+       }
+       )
+        }
+        </div>
+      
+        </>
+
+      );
+    }
+export default Main;
