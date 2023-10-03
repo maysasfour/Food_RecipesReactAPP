6acc9a58bd0a7d38ed090add6b05d83fba5ea662
maysasfour <mays.suhail@gmail.com>
2023-10-03 11:21:57 +0300

    errors solved

--- src/header.js
+++ src/header.js
@@ -12,7 +12,7 @@ function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#browse">Browse</Nav.Link>
+            <Nav.Link href="/browse">Browse</Nav.Link>
         
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
